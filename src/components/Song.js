fd8dd6de42bdae06895be00aeacda2f86a9527b7
Aoi Kurokawa <aoikurokawa@Aois-MacBook-Air.local>
2021-02-04 23:04:14 +0700

    create UI and data

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+
+const Song = () => {
+    return(
+        <div className="song-container">
+            <h1>Picture</h1>
+            <h1>Artist</h1>
+            <h1>Songs</h1>
+        </div>
+    )
+}
+
+export default Song;
